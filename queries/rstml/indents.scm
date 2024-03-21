f3eb2c26a80fe7af99cf9e4f46ce064be353aef0
Ryan Halliwell <git@rayliwell.com>
2024-03-21 21:18:06 +0000

    Dedent cursor after end tag

--- queries/rstml/indents.scm
+++ queries/rstml/indents.scm
@@ -4,12 +4,17 @@
 
 (element_node) @indent.begin
 
+(self_closing_element_node) @indent.begin
+
 (open_tag
   ">" @indent.branch)
-
-(self_closing_element_node) @indent.begin
 
 (self_closing_element_node
   "/>" @indent.branch)
 
 (close_tag) @indent.branch
+
+[
+  ">"
+  "/>"
+] @indent.end
